a1e4e85af6e57dc50a41e31b99bee9dd8f6b2c8c
Lucas Cunha <lucas71cunha@gmail.com>
2025-01-15 23:28:29 -0300

    pesquisar, controller

--- src/main/java/io/github/spring/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/spring/libraryapi/controller/AutorController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -66,5 +68,21 @@ public class AutorController {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping
+    public ResponseEntity <List<AutorDTO>> pesquisar(
+            @RequestParam (value = "nome", required = false) String nome,
+            @RequestParam (value = "nome", required = false) String nacionalidade) {
+        List<Autor> resultado = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> lista = resultado
+                .stream()
+                .map(autor -> new AutorDTO(
+                        autor.getId(),
+                        autor.getNome(),
+                        autor.getDataNascimento(),
+                        autor.getNacionalidade())
+                ).collect(Collectors.toList());
+        return ResponseEntity.ok(lista);
     }
 }
--- src/main/java/io/github/spring/libraryapi/service/AutorService.java
+++ src/main/java/io/github/spring/libraryapi/service/AutorService.java
@@ -4,8 +4,10 @@ import io.github.spring.libraryapi.model.Autor;
 import io.github.spring.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class AutorService {
@@ -20,12 +22,27 @@ public class AutorService {
     public Autor salvar(Autor autor) {
         return repository.save(autor);
     }
+
     public Optional<Autor> obterPorId(UUID id) {
         return repository.findById(id);
     }
 
-
     public void deletar(Autor autor) {
         repository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade) {
+        List<Autor> autores = repository.findAll();
+
+        if (nome != null) {
+            autores = autores.stream()
+                    .filter(autor -> autor.getNome().equalsIgnoreCase(nome))
+                    .collect(Collectors.toList()); }
+        if (nacionalidade != null) {
+            autores = autores.stream()
+                    .filter(autor -> autor.getNacionalidade().equalsIgnoreCase(nacionalidade))
+                    .collect(Collectors.toList());
+        }
+        return autores;
     }
 }
